5feaff0e560152a22dcc3b8594490b38800f0b32
Rajjya Rohan Paudyal <84804921+rajjyarohan10@users.noreply.github.com>
2025-01-04 20:13:00 -0500

    Fix static export and deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export", // Enable static export
+  basePath: "/next-report-app", // Required for Github Pages
 };
 
 export default nextConfig;
